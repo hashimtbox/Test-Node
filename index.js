9a64f7b2fb4c3f3fa1fb2085bd95ab3d472c8412
HamzaAzeem97 <hamzaazeem.tbox@gmail.com>
2021-03-17 17:20:25 +0500

    port changed

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ app.get("/", function(req, res) {
     res.send("hello");
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Express server listening on port %d in %s mode");
+    
+  });
